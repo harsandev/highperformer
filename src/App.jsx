6d58e4ae795eec6d51a7d2a7f2665070542f050d
Bala <bala@tibura.com>
2025-05-13 11:46:01 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
 const handleExtract = async () => {
     setLoading(true);
     try {
-      const response = await fetch('http://localhost:5000/api/extract', {
+      const response = await fetch('https://synergy-cf-api.axodesk.com:3000/api/extract', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ url }),
